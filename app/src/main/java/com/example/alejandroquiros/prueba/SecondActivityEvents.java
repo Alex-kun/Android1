07c65408447ba3471ebf1daabef33b4874eeff78
Alex-Kun <alejandro.quiros@live.u-tad.com>
2017-11-13 22:12:20 +0100

    Vista 2

--- app/src/main/java/com/example/alejandroquiros/prueba/SecondActivityEvents.java
+++ app/src/main/java/com/example/alejandroquiros/prueba/SecondActivityEvents.java
@@ -1,10 +1,22 @@
 package com.example.alejandroquiros.prueba;
 
+import android.content.Intent;
+import android.view.View;
+
 /**
  * Created by alejandroquiros on 10/11/17.
  */
 
-public class SecondActivityEvents {
+public class SecondActivityEvents implements View.OnClickListener{
 
+    private SecondActivity secondActivity;
+    public SecondActivityEvents(SecondActivity secondActivity){
+        this.secondActivity = secondActivity;
+    }
 
-}
+    @Override
+    public void onClick(View view) {
+
+        }
+    }
+
