c976a8a44da06e007414d0268c9334bce0601747
Alex-Kun <alejandro.quiros@live.u-tad.com>
2017-11-14 09:55:50 +0100

    code view 2

--- app/src/main/java/com/example/alejandroquiros/prueba/SecondActivity.java
+++ app/src/main/java/com/example/alejandroquiros/prueba/SecondActivity.java
@@ -2,8 +2,12 @@ package com.example.alejandroquiros.prueba;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    public Button editbtn;
+    public Button backbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
